adda697c94d9670a0afc82d9884880ea56f634c3
arezvanov@arezvanov <adel.rezvanov@noveogroup.com>
2023-01-29 09:06:07 +0400

    S7-L38 Button component with svg icon support

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,14 @@
 import type { NextPage } from "next";
-import { Htag } from "../components";
+import { Button, Htag } from "../components";
 
 const Home: NextPage = () => {
   return (
     <div>
       <Htag tag="h1">Text</Htag>
+      <Button appearance="primary">Button</Button>
+      <Button appearance="ghost" arrow="down">
+        Button
+      </Button>
     </div>
   );
 };
