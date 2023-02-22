0caa4014133e24d547b2cff1171062441f913c70
arezvanov@arezvanov <adel.rezvanov@noveogroup.com>
2023-02-22 17:04:06 +0400

    S8-L57 layout base components

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,40 +1,43 @@
 import type { NextPage } from "next";
 import { useState } from "react";
 import { Button, Htag, Ptag, Rating, Tag } from "../components";
+import { Layout } from "../layout/Layout";
 
 const Home: NextPage = () => {
   const [rating, setRating] = useState<number>(0);
   return (
-    <div>
-      <Htag tag="h1">Text</Htag>
-      <Button appearance="primary">Button</Button>
-      <Button appearance="ghost" arrow="down">
-        Button
-      </Button>
-      <Ptag size="l">
-        Выше указаны программы Adobe InDesign, Adobe Illustrator, Corel Draw и
-        ими можно успешно пользоваться дома или в дороге. Современные ноутбуки
-        хорошо справляются с нагрузкой, так зачем загонять специалиста в душный
-        офис. В этой профессии важным считается вдохновение, поэтому дизайнеры
-        ищут его в разных местах.
-      </Ptag>
-      <Tag size="s" color="ghost">
-        small ghost
-      </Tag>
-      <Tag size="m" color="gray">
-        medium gray
-      </Tag>
-      <Tag size="s" color="green">
-        10 green
-      </Tag>
-      <Tag size="m" color="primary">
-        10 primary
-      </Tag>
-      <Tag size="s" color="red" href="https://google.com">
-        10 red
-      </Tag>
-      <Rating rating={rating} setRating={setRating} isEditable />
-    </div>
+    <Layout>
+      <div>
+        <Htag tag="h1">Text</Htag>
+        <Button appearance="primary">Button</Button>
+        <Button appearance="ghost" arrow="down">
+          Button
+        </Button>
+        <Ptag size="l">
+          Выше указаны программы Adobe InDesign, Adobe Illustrator, Corel Draw и
+          ими можно успешно пользоваться дома или в дороге. Современные ноутбуки
+          хорошо справляются с нагрузкой, так зачем загонять специалиста в
+          душный офис. В этой профессии важным считается вдохновение, поэтому
+          дизайнеры ищут его в разных местах.
+        </Ptag>
+        <Tag size="s" color="ghost">
+          small ghost
+        </Tag>
+        <Tag size="m" color="gray">
+          medium gray
+        </Tag>
+        <Tag size="s" color="green">
+          10 green
+        </Tag>
+        <Tag size="m" color="primary">
+          10 primary
+        </Tag>
+        <Tag size="s" color="red" href="https://google.com">
+          10 red
+        </Tag>
+        <Rating rating={rating} setRating={setRating} isEditable />
+      </div>
+    </Layout>
   );
 };
 
