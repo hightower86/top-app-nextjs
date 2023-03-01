07312941a23c18438f1088545d1cc7d13ff28871
arezvanov@arezvanov <adel.rezvanov@noveogroup.com>
2023-03-01 21:52:55 +0400

    S10-L66 Footer component

--- layout/Footer/Footer.tsx
+++ layout/Footer/Footer.tsx
@@ -1,5 +1,21 @@
 import { FooterProps } from "./Footer.props";
+import cls from "./Footer.module.css";
+import cn from "classnames";
+import { format } from "date-fns";
 
-export const Footer = (props: FooterProps): JSX.Element => {
-  return <div {...props}>Footer</div>;
+export const Footer = ({
+  className,
+  ...otherProps
+}: FooterProps): JSX.Element => {
+  return (
+    <footer className={cn(cls.footer, className)} {...otherProps}>
+      <div>OwTop 2022-{format(new Date(), "yyyy")} All rights reserved</div>
+      <a href="#" target="_blank">
+        User agreement
+      </a>
+      <a href="#" target="_blank">
+        Privacy Policy
+      </a>
+    </footer>
+  );
 };
