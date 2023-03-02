a6ae8fe7e11db69b757c857813a9182d467dd68d
arezvanov@arezvanov <adel.rezvanov@noveogroup.com>
2023-03-02 23:41:05 +0400

    S10-L71 static pages generating

---
+++ interfaces/page.interface.ts
@@ -0,0 +1,64 @@
+export enum TopLevelCategory {
+  Courses,
+  Services,
+  Books,
+  Products,
+}
+
+export interface TopPageAdvantage {
+  title: string;
+  description: string;
+  _id: string;
+}
+
+export interface HhDAta {
+  count: number;
+  juniorSalary: number;
+  middleSalary: number;
+  seniorSalary: number;
+  updatedAt: Date;
+  _id: string;
+}
+
+export interface Blog {
+  h1: string;
+  metaTitle: string;
+  metaDescription: string;
+  views: number;
+  _id: string;
+}
+
+export interface Sravnikus {
+  metaTitle: string;
+  metaDescription: string;
+  _id: string;
+}
+
+export interface Learningclub {
+  metaTitle: string;
+  metaDescription: string;
+  _id: string;
+}
+
+export interface TopPageModel {
+  _id: string;
+  tags: string[];
+  secondCategory: string;
+  alias: string;
+  title: string;
+  category: string;
+  seoText: string;
+  tagsTitle: string;
+  metaTitle: string;
+  metaDescription: string;
+  firstCategory: number;
+  TopPageadvantages: TopPageAdvantage[];
+  createdAt: Date;
+  updatedAt: Date;
+  __v: number;
+  hhDAta: HhDAta;
+  categoryOn: string;
+  blog: Blog;
+  sravnikus: Sravnikus;
+  learningclub: Learningclub;
+}
