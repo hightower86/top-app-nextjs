3a601468e1cb5a72b39257c3e4c2859c39596cfb
arezvanov@arezvanov <adel.rezvanov@noveogroup.com>
2023-04-18 20:14:00 +0400

    TextArea component

---
+++ components/TextArea/TextArea.tsx
@@ -0,0 +1,15 @@
+import { TextAreaProps } from "./TextArea.props";
+import cn from "classnames";
+import styles from "./TextArea.module.css";
+
+export const TextArea = ({
+  className,
+  ...props
+}: TextAreaProps): JSX.Element => {
+  return (
+    <textarea
+      className={cn(className, styles.textArea)}
+      {...props}
+    />
+  );
+};
