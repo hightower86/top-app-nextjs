200a5b7b102c0680851c7b227af05dd5dd9d0c4e
arezvanov@arezvanov <adel.rezvanov@noveogroup.com>
2023-04-25 21:34:43 +0400

    Accessibility from keyboard

--- layout/Layout.tsx
+++ layout/Layout.tsx
@@ -1,19 +1,48 @@
 import { LayoutProps } from "./Layout.props";
+import cn from "classnames";
 import { Header } from "./Header/Header";
 import { Sidebar } from "./Sidebar/Sidebar";
 import { Footer } from "./Footer/Footer";
-import { FC } from "react";
+import { FC, useRef, useState, KeyboardEvent } from "react";
 import cls from "./Layout.module.css";
 import { AppContext, AppContextProvider } from "../context/app.context";
 import { Up } from "../components";
 
 const Layout = ({ children }: LayoutProps): JSX.Element => {
+  const [isSkipLinkDisplayed, setIsSkipLinkDisplayed] =
+    useState<boolean>(false);
+  const bodyRef = useRef<HTMLDivElement>(null);
+
+  const skipContentAction = (key: KeyboardEvent) => {
+    if (key.code == "Space" || key.code == "Enter") {
+      key.preventDefault();
+      bodyRef.current?.focus();
+    }
+    setIsSkipLinkDisplayed(false);
+  };
+
   return (
     <div className={cls.wrapper}>
+      <a
+        onFocus={() => setIsSkipLinkDisplayed(true)}
+        tabIndex={1}
+        className={cn(cls.skipLink, {
+          [cls.displayed]: isSkipLinkDisplayed,
+        })}
+        onKeyDown={skipContentAction}
+      >
+        Сразу к содержанию
+      </a>
       <Header className={cls.header} />
 
       <Sidebar className={cls.sidebar} />
-      <div className={cls.body}>{children}</div>
+      <div
+        tabIndex={0}
+        className={cls.body}
+        ref={bodyRef}
+      >
+        {children}
+      </div>
 
       <Footer className={cls.footer} />
       <Up />
