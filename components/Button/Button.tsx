a256967ffa883e5cb92c6510193ce678a0c23e34
arezvanov@arezvanov <adel.rezvanov@noveogroup.com>
2023-04-25 13:47:07 +0400

    Button animation

--- components/Button/Button.tsx
+++ components/Button/Button.tsx
@@ -2,6 +2,7 @@ import { ButtonProps } from "./Button.props";
 import ArrowIcon from "./ArrowIcon.svg";
 import cn from "classnames";
 import styles from "./Button.module.css";
+import { motion } from "framer-motion";
 
 export const Button = ({
   children,
@@ -11,12 +12,13 @@ export const Button = ({
   ...props
 }: ButtonProps) => {
   return (
-    <button
+    <motion.button
+      whileHover={{ scale: 1.05 }}
       className={cn(styles.button, styles[appearance], className)}
       {...props}
     >
       {children}
       <ArrowIcon className={cn(styles[arrow], styles.arrow, className)} />
-    </button>
+    </motion.button>
   );
 };
